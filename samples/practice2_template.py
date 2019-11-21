2cef12288b7ca36cf4a2dc51f8bfd22b9b4878e2
Evgenii Vasiliev <eugene.unn@gmail.com>
2019-11-21 12:25:02 +0300

    Add detector solution

--- samples/practice2_template.py
+++ samples/practice2_template.py
@@ -1,6 +1,8 @@
 ﻿"""
 
 Inference engine detector sample
+
+python practive2_template.py -c ../models/mobilenet-ssd.xml -w ../models/mobilenet-ssd.bin -i ../images/dog.jpg -d CPU -l "C:\Program Files (x86)\IntelSWTools\openvino\inference_engine\bin\intel64\Release\cpu_extension_avx2.dll"
  
 """
 import sys
@@ -13,21 +15,40 @@ from ie_detector import InferenceEngineDetector
 def build_argparse():
     parser = argparse.ArgumentParser()
     
-    #
-    # Add your code here
-    #
-    
+    parser.add_argument('-c', '--config', help = 'Path to .xml model file', 
+                        type = str)
+    parser.add_argument('-w', '--weights', help = 'Path to .bin model file', 
+                        type = str)
+    parser.add_argument('-l', '--cpu_extension', help = 'Optional. Required for CPU custom layers. Absolute path to a shared library with the kernels implementations.',    
+                        default = '', type = str)
+    parser.add_argument('-d', '--device', help = 'Execution device', 
+                        type=str, default = 'CPU')
+    parser.add_argument('-i', '--input', help = 'Input argument', 
+                        type = str)
     return parser
 
 def main():
-    log.basicConfig(format = "[%(levelname)s] %(message)s", level = log.INFO, 
+    log.basicConfig(format = '[%(levelname)s] %(message)s', level = log.INFO, 
                                stream = sys.stdout)
-    log.info("Hello object detection!")
+    log.info('Hello object detection!')
     args = build_argparse().parse_args()
     
-    #
-    # Add your code here
-    #
+    image_src = cv2.imread(args.input)
+    log.info('Loaded image with size {}'.format(image_src.shape))
+    
+    detector = InferenceEngineDetector(args.weights, args.config, args.device, args.cpu_extension)
+    log.info('Loaded dl net')
+    
+    detect_result = detector.detect(image_src)
+    log.info('Objects are detected')
+    
+    detect_image = detector.draw_detection(detect_result, image_src)
+    log.info('Objects are drown')
+    
+    cv2.imshow('Detections', detect_image)
+    cv2.waitKey(0) # waits until a key is pressed
+
+    cv2.destroyAllWindows() 
     
     return 
 
--- src/ie_detector.py
+++ src/ie_detector.py
@@ -10,33 +10,42 @@ from openvino.inference_engine import IENetwork, IECore
 class InferenceEngineDetector:
     def __init__(self, weightsPath = None, configPath = None,
                  device = 'CPU', extension = None):
-        #
-        # Add your code here
-        #
-        
+        self.weights = weightsPath
+        self.config = configPath
+        self.ie = IECore()
+        self.net = IENetwork(model = configPath, weights = weightsPath)
+        if extension:
+            self.ie.add_extension(extension, 'CPU')
+        self.exec_net = self.ie.load_network(network = self.net, 
+                                             device_name = device)
         return
 
     def draw_detection(self, detections, img):
-    
-        #
-        # Add your code here
-        #
-        
+        (h, w) = img.shape[:2]
+        for i in range(0, detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > 0.5:
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+                text = "{:.2f}%".format(confidence * 100)
+                y = startY - 10 if startY - 10 > 10 else startY + 10
+                cv2.rectangle(img, (startX, startY), (endX, endY),
+                              (0, 255, 0), 2)
+                cv2.putText(img, text, (startX, y),
+                            cv2.FONT_HERSHEY_COMPLEX, 0.45, (0, 0, 255), 1)
         return img
 
     def _prepare_image(self, image, h, w):
-    
-        #
-        # Add your code here
-        #
-        
+        if image.shape[:-1] != (h, w):
+            image = cv2.resize(image, (w, h))
+        image = image.transpose((2, 0, 1))
         return image
         
     def detect(self, image):
-    
-        #
-        # Add your code here
-        #
-        detection = None
-        
+        input_blob = next(iter(self.net.inputs))
+        out_blob = next(iter(self.net.outputs))
+        n, c, h, w = self.net.inputs[input_blob].shape
+        blob = self._prepare_image(image, h, w)
+        output = self.exec_net.infer(inputs={input_blob: blob})
+        detection = output[out_blob]
         return detection
